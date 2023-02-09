d931139d901cd424f52919e62b43d71c6d4ff7d5
Ryne Burden <ryneburden@protonmail.com>
2023-02-08 23:18:29 -0500

    move to class for wavfile processing

--- test.py
+++ test.py
@@ -3,29 +3,61 @@ from scipy.io import wavfile
 import numpy as np
 import scipy.io
 import math
+from typing import List
 
-dtypes = {
-    "float32": "32_bit_floating_point",
-    "int32": "32-bit integer PCM",
-}
 
-samplerate, data = wavfile.read("test.wav")
+class WavFile:
+    def __init__(self, filename: str, fps: int) -> None:
+        self.sample_rate, self.audio_data = wavfile.read(filename)
 
-print(f"number of channels = {data.shape[1]}")
+        self.duration_in_seconds = math.floor(len(self.audio_data) / self.sample_rate)
+        self.fps = fps if fps == 30 or fps == 60 else 60
+        self.bits_per_frame = math.floor(self.sample_rate / self.fps)
 
-length = data.shape[0] / samplerate
-print(f"length = {length}s")
+        # This is how much data we will have to vectorize for each video frame within a second of audio
+        #   One second of audio has (sample_rate) values that need to be divided
+        self.data_point_per_frame = math.floor(self.sample_rate / self.fps)
 
-channel_1_info = data[:, 0]
-channel_2_info = data[:, 1]
+    def __get_data_for_each_second(self):
+        scanner_length = self.sample_rate
+        scanner_start_cursor = 0
+        scanner_end_cursor = scanner_start_cursor + scanner_length
+        data_by_second = []
+        for seconds_index in range(0, self.duration_in_seconds):
+            data_by_second.append(
+                [*self.audio_data[scanner_start_cursor:scanner_end_cursor]]
+            )
 
-frames_per_second = 60
+            scanner_start_cursor += scanner_length
+            scanner_end_cursor += scanner_length
 
-scanner_length = samplerate
-scanner_begin = 0
-scanner_end = scanner_begin + scanner_length
+        return data_by_second
 
-number_of_seconds = math.floor(len(channel_1_info) / scanner_length)
-number_of_divisions = math.floor(samplerate / frames_per_second)
+    def __get_frames_data_for_one_second(self, second_queried):
+        scanner_begin = second_queried * self.bits_per_frame
+        scanner_end = scanner_begin + self.bits_per_frame
 
-frames_data = []
+        frames_data = []
+        for frame_index in range(0, self.fps):
+            frames_data.append([*self.audio_data[scanner_begin:scanner_end]])
+
+            scanner_begin += self.bits_per_frame
+            scanner_end += self.bits_per_frame
+
+        return frames_data
+
+    def get_formatted_audio_data(self):
+        formatted_data = []
+        for second_index in range(0, self.duration_in_seconds):
+            formatted_data.append(self.__get_frames_data_for_one_second(second_index))
+
+        return formatted_data
+
+
+def main():
+    file = WavFile("test.wav", 60)
+    data = file.get_formatted_audio_data()
+
+
+if __name__ == "__main__":
+    main()
